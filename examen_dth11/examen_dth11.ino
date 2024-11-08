08ed0fc56cf96a8092fb887db843de12e562e158
WhiteBryz <davidflores@live.com.mx>
2024-11-08 09:02:33 -0600

    Added DHT example; Se añade ejemplo del sensor de Humedad y Temperatura

---
+++ examen_dth11/examen_dth11.ino
@@ -0,0 +1,74 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+const char* ssid = "INFINITUMCA97_2.4_EXT";
+const char* password = "GlSwVBuNxT";
+const char* mqtt_server = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  dht.begin();
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    Serial.println("Se desconecto.");
+    reconnect();
+  }
+
+  client.loop();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  String payload = "{\"temperature\":" + String(t) + ",\"humidity\":" + String(h) + "}";
+  // Envío de datos
+  client.publish("dafc/temperatura", (char*) payload.c_str());
+  Serial.println("Envió datos");
+  delay(2000);
+}
